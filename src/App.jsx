7a272b921d12be26a85ef512e48da51c0c57972e
RebecaTI <mebuffbeka@gmail.com>
2025-02-14 20:25:16 -0300

    adicionando card

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { useEffect, useState } from 'react';
 import Search from './components/Search';
 import Spinner from './components/Spinner'
 import MovieCard from './components/MovieCard';
+import { useDebounce } from 'react-use';
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
-
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 const API_OPTIONS = {
@@ -20,6 +20,11 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState('');
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
+
+  //Debounce the search term to prevent making too many API requests
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 500
+    , [searchTerm]);
 
   const fetchMovies = async (query = '') => {
     setIsLoading(true);
